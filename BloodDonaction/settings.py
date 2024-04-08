234b4466fc3482ee44ae79d657327591f904eac8
Cᵣₑₐₜᵢᵥₑ Gₒᵤᵣₐᵥ <120663141+HYDRAGOURAV@users.noreply.github.com>
2024-04-08 15:40:57 +0530

    Update settings.py
    add static Files

--- BloodDonaction/settings.py
+++ BloodDonaction/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+# add static files 
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
